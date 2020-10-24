fa2d8da7838bb2c1e2015f6eb9ba77500e0a6f8f
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-24 18:30:10 -0400

    Query github with fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, { useState, useEffect } from 'react';
 
+import fetchGraphQL from './fetchGraphQL';
 
-function App() {
+export default () => {
+	const [state, setState] = useState({ name: '' });
+
+	useEffect(() => {
+		let isMounted = true;
+		(async () => {
+			try {
+				const response = await fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+		`);
+				if (!isMounted) return;
+				setState({ name: response.data.repository.name });
+			} catch (e) {
+				console.log(e);
+			}
+		})();
+		return () => {
+			isMounted = false;
+		};
+	}, [fetchGraphQL]);
+
 	return (
 		<div className='App'>
-      Hey
+			{state.name ? `Repository: ${state.name}` : 'Loading'}
 		</div>
 	);
-}
-
-export default App;
+};
